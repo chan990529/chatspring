45d6f21be755484959e8bbaeb768e82354932f87
CHAN <chan990529@gmail.com>
2024-11-11 20:52:11 +0900

    Changes(api 수정, avg_reach_time 추가)

--- src/main/java/com/chatspring/chatspring/WebConfig.java
+++ src/main/java/com/chatspring/chatspring/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-              .allowedOrigins("https://13.209.60.49:80", "https://scalping.app", "https://www.scalping.app")
+              .allowedOrigins("http://13.209.60.49:80", "https://scalping.app", "https://www.scalping.app")
 //                .allowedOrigins("http://localhost:3000", "https://scalping.app", "https://www.scalping.app") // 리액트 앱의 주소
 //                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
