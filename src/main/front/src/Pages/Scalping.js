e65c8c6c9cd2f404df4005ee03c7836dbadf11ac
CHAN <chan990529@gmail.com>
2024-11-15 00:06:54 +0900

    Changes(Top button)

--- src/main/front/src/Pages/Scalping.js
+++ src/main/front/src/Pages/Scalping.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useImperativeHandle } from 'react';
 import axios from 'axios';
 import { Card, CardContent, Typography, Grid, TextField, useMediaQuery, Avatar, Box } from '@mui/material';
 import CloseImage from './Close.png';
