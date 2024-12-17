13a19edc28b4018b709bc3450f4a7b33145808cf
CHAN <chan990529@gmail.com>
2024-12-18 00:52:10 +0900

    Changes(로그인 업데이트)

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Layout from './Component/Layout'; // Layout을 import
 import ScalpingPage from './Pages/Scalping';
