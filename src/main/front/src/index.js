2df0b2dfd5f2f4af90185ea6d4a48cdadae61b8d
CHAN <chan990529@gmail.com>
2024-12-18 20:43:48 +0900

    Changes(Service Worker 추가)

--- src/main/front/src/index.js
+++ src/main/front/src/index.js
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import theme from './theme';  // 만든 테마 파일 import
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +19,20 @@ root.render(
       </ThemeProvider>
   </React.StrictMode>
 );
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function(registration) {
+            console.log('Service Worker 등록 성공:', registration);
+        })
+        .catch(function(error) {
+            console.error('Service Worker 등록 실패:', error);
+        });
+}
+
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
